5574830c545f961046fa56ace8cf451a4b188a9b
Jared Healy <healy_jared@yahoo.com>
2024-11-03 16:00:06 +0800

    minor changes

--- components/HandballComponenets/ServerActions.ts
+++ components/HandballComponenets/ServerActions.ts
@@ -1,2 +1,3 @@
-// export const SERVER_ADDRESS = 'https://squarers.org:443';
-export const SERVER_ADDRESS = 'http://localhost:80';
+export const SERVER_ADDRESS = 'https://squarers.org:443';
+// export const SERVER_ADDRESS = 'http://localhost:25565';
+// export const SERVER_ADDRESS = 'http://49.192.25.179:25565';
