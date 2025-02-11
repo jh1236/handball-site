2d5ca7b7ecf347bfdafff7f364f1e0efbc69ee3e
thatgamerdood1402 <thatgamerdood1402@gmail.com>
2025-02-11 19:45:40 +0800

    undeleted the scoreboard

---
+++ app/games/[game]/scoreboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { GamePageArgs } from '@/app/types';
+import { Scoreboard } from '@/components/HandballComponenets/GamePageComponents/Scoreboard';
+
+export default async function GamesPage({ params }: GamePageArgs) {
+  const { game } = await params;
+
+  return (
+      <Scoreboard gameID={+game}></Scoreboard>
+  );
+}
