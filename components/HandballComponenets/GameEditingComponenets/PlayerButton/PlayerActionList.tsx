893483cbb62404be0e89f256c1f6c42d3e9f5972
Jared Healy <healy_jared@yahoo.com>
2025-11-17 15:26:50 +0800

    Adds break

--- components/HandballComponenets/GameEditingComponenets/PlayerButton/PlayerActionList.tsx
+++ components/HandballComponenets/GameEditingComponenets/PlayerButton/PlayerActionList.tsx
@@ -556,7 +556,12 @@ export function PlayerActionList({
         color: 'white',
         content: (
           <>
-            {!isVertical && <Title order={3}>Swap with {a.get?.name}</Title>}
+            {!isVertical && (
+              <>
+                <Title order={3}>Swap with {a.get?.name}</Title>
+                <br />
+              </>
+            )}
             <Button
               size="lg"
               onClick={() => {
